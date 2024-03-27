5db32bd1b2384d2d60c8ed401be498d627e1bd30
Anatolii Malibroda <tolikmalibroda@gmail.com>
2024-03-27 22:26:43 +0100

    Added LSP for jsonnet.

--- lua/custom/plugins/lsp.lua
+++ lua/custom/plugins/lsp.lua
@@ -145,7 +145,7 @@ return { -- LSP Configuration & Plugins
       --
       -- But for many setups, the LSP (`tsserver`) will work just fine
       tsserver = {},
-      --
+      jsonnet_ls = {},
 
       lua_ls = {
         -- cmd = {...},
